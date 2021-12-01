4a493eb84d5ccae5f4c87dc46291f7d1f9a1a348
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-12-01 13:48:42 +0800

    增加点赞弹性动画

--- basic functions/basic functions/NewsCell.swift
+++ basic functions/basic functions/NewsCell.swift
@@ -17,7 +17,31 @@ class NewsCell: UITableViewCell{
     @IBOutlet var commentCount: UILabel!
     @IBOutlet var likeCount: UILabel!
     @IBOutlet var content: UILabel!
+
+    private func likeAnimation(button:UIButton){
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping:0.5, initialSpringVelocity: 0.5,options: [.allowUserInteraction],
+        animations: {
+            button.transform = CGAffineTransform(scaleX: 2.5, y: 2.5)
+            button.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            button.setImage(UIImage(named: "emoji_52"), for: .normal)
+        }, completion:nil)
+        
+    }
     
+    @IBAction func like(_ sender: UIButton){
+        
+        sender.isSelected = !sender.isSelected
+        print(sender.isSelected)
+        if sender.isSelected{
+           likeAnimation(button: sender)
+        }
+        else{
+            sender.setImage(UIImage(named: "u13_like_feed"), for: .normal)
+//            sender.layer.removeAllAnimations()
+//            sender.layer.transform = CATransform3DIdentity
+        }
+        
+    }
 }
 
 extension NewsCell{
